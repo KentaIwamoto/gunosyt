aac4c55a76ef97c6585c60e617ea02ef298e5ca8
KentaIwamoto <iwamochan1014@gmail.com>
2016-12-05 02:13:59 +0900

    form作成

--- classifier/naive_bayes/urls.py
+++ classifier/naive_bayes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.hello_world, name='hello_world'),
+    url(r'^template/$', views.hello_template, name='hello_template'),  # 追加する
+    url(r'^if/$', views.hello_if, name='hello_if'), # 追加する
+    url(r'^get/$', views.hello_get_query, name='hello_get_query'), # 追加する
 ]
--- classifier/naive_bayes/views.py
+++ classifier/naive_bayes/views.py
@@ -1,6 +1,29 @@
 # Create your views here.
+from datetime import datetime  # 追加する
 from django.http.response import HttpResponse
+from django.shortcuts import render # 追加する
+
 
 
 def hello_world(request):
     return HttpResponse('Hello World!')
+
+def hello_template(request):
+    d = {
+        'hour': datetime.now().hour,
+        'message': 'Sample message',
+    }
+    return render(request, 'index.html', d)
+
+def hello_if(request):
+    d = {
+        'is_visible': False,
+        'empty_str': 'hh',
+    }
+    return render(request, 'if.html', d)
+
+def hello_get_query(request):
+    d = {
+        'url': request.GET.get('url')
+    }
+    return render(request, 'get_query.html', d)
