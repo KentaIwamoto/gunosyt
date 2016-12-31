c2ecd70bd8024efe775611edce8726bfe34a8c3e
KentaIwamoto <iwamochan1014@gmail.com>
2016-12-31 17:25:02 +0900

    分類器をアプリケーションに組み込み完了

--- classifier/naive_bayes/urls.py
+++ classifier/naive_bayes/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello_world, name='hello_world'),
-    url(r'^template/$', views.hello_template, name='hello_template'),  # 追加する
-    url(r'^if/$', views.hello_if, name='hello_if'), # 追加する
-    url(r'^get/$', views.hello_get_query, name='hello_get_query'), # 追加する
+    # url(r'^$', views.hello_world, name='hello_world'),
+    # url(r'^template/$', views.hello_template, name='hello_template'),  # 追加する
+    # url(r'^if/$', views.hello_if, name='hello_if'), # 追加する
+    # url(r'^get/$', views.hello_get_query, name='hello_get_query'), # 追加する
+    # url(r'^time/$',views.time, name='time'),
+    url(r'^result/$',views.result, name='result'),
+    url(r'^urlform/$',views.urlform, name='urlform'),
+
 ]
